b936bd860dd88682de20664831c4d532e9d52037
Amir <1644943+pwnrazr@users.noreply.github.com>
2022-03-15 18:37:06 +0800

    Initial commit
    Minimal intended functionality

---
+++ app/src/main/java/com/pwnrazr/virusscanner/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.pwnrazr.virusscanner
+
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val doingText: TextView = findViewById(R.id.doing_text)
+        val virusAmtText: TextView = findViewById(R.id.virus_amount_text)
+        val button: Button = findViewById(R.id.scan_button)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+
+        var isScanning = false
+
+        var randomNum = 0
+
+        val startUpMessage = AlertDialog.Builder(this)
+        startUpMessage.setTitle("Welcome")
+        startUpMessage.setMessage("Thank you for using Virus Scanner by Pwnrazr")
+
+        startUpMessage.setPositiveButton("Okay") { dialog, which ->
+        }
+        startUpMessage.show()
+
+        fun animateTextView(initialValue: Int, finalValue: Int, textview: TextView) {
+            val valueAnimator = ValueAnimator.ofInt(initialValue, finalValue)
+            textview.text = "0"
+            valueAnimator.duration = 1500 //ms
+            valueAnimator.addUpdateListener { valueAnimator ->
+                textview.text = valueAnimator.animatedValue.toString()
+            }
+            valueAnimator.start()
+        }
+
+        class SimpleRunnable: Runnable {
+            override fun run() {
+                while(!Thread.currentThread().isInterrupted && isScanning)
+                {
+                    if(virusAmtText.text == randomNum.toString())
+                    {
+                        Thread.currentThread().interrupt()
+                        isScanning = false
+
+                        doingText.text = "Scanning complete"
+                        progressBar.visibility = View.INVISIBLE
+                        Log.d("myApp","Stopped")
+                    }
+                    Log.d("myApp","Running")
+                }
+            }
+        }
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        button.setOnClickListener {
+            if(!isScanning) {
+                virusAmtText.text = "0"
+                randomNum = (86..523).random()
+
+                doingText.text = "Scanning device"
+                progressBar.visibility = View.VISIBLE
+
+                isScanning = true
+
+                animateTextView(0, randomNum, virusAmtText)
+                val threadWithRunnable = Thread(SimpleRunnable())
+                threadWithRunnable.start()
+            }
+            else {
+                Toast.makeText(applicationContext,"Scan ongoing",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
